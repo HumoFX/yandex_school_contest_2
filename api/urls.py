67b87cdffae5f912f5b085bf7d0097c9eea18145
humofx <humo_oatahanov@icloud.com>
2021-03-29 19:17:34 +0500

    collect static

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from api.views import OrderListCreateAPIView, CouriersPatchAPIView, CouriersListCreateAPIView, OrderAssignAPIView, OrderCompleteAPIView
+from yandex_school_contest_2 import settings
 
 urlpatterns = [
     path('couriers', CouriersListCreateAPIView.as_view()),
@@ -9,4 +11,5 @@ urlpatterns = [
     path('orders/assign', OrderAssignAPIView.as_view()),
     path('orders/complete', OrderCompleteAPIView.as_view()),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#echo "Collect static files"
-#python3 manage.py collectstatic --noinput
+echo "Collect static files"
+python3 manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
